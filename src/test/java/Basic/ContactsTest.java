4a32d38300f5675f83b05397f26482aa4703c12c
BhagyashreeNimbalkar24 <150345152+BhagyashreeNimbalkar24@users.noreply.github.com>
2024-03-16 13:15:46 +0530

    updated project

---
+++ src/test/java/Basic/ContactsTest.java
@@ -0,0 +1,15 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ContactsTest {
+	
+public void CreateContactsTest() {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.ajio.com/");
+	}
+
+}
